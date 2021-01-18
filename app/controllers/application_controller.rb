80e78f376bbb8c03f482ae66256b7da457e03980
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-18 17:32:31 +0900

    コメントアウトの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # devise_parameter_sanitizer.permit(追加したいメソッドの種類, keys: [追加したいパラメーター名] 
   def configure_permitted_parameters
-    # binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [
       :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
