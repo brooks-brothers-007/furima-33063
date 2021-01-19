42e4e13422dc4c82145b54b56ed26f891b765a81
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-19 12:53:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to       :user 
   has_one          :purchase 
+
+  validates :image, :name, :information, :category_id, :sales_status_id, :shipping_fee_status_id
+            , :prefecture_id, :scheduled_delivery_id, :price, presence: true
 end
