aae0895fee616f68e003c9b3e80854378fad0688
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-21 20:59:36 +0900

    販売手数料や販売利益の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:new,:create]
   def index
+    
   end  
   def new
     @item = Item.new
---
+++ app/javascript/item_price.js
@@ -0,0 +1,27 @@
+function count (){  
+  // 金額を入力した数値をpriceInputという変数に格納
+  const priceInput = document.getElementById("item-price");
+  // 入力するたびにイベントを発火できるようにする
+  priceInput.addEventListener("input", () => {
+
+    // input要素から実際に入力した金額の値を取得
+    const inputValue = priceInput.value;
+    console.log(inputValue)
+
+    // 販売手数料に書き換え、表示する
+    const addTaxDom = document.getElementById("add-tax-price");
+    console.log(addTaxDom)
+    // innerHTMLで、HTML要素の書き換えを行う。また、入力した金額をもとに販売手数料を計算する処理
+    addTaxDom.innerHTML =  Math.floor(inputValue * 0.1);
+
+    // 販売利益に書き換え、表示する
+    const priceContent = document.getElementById("profit");
+    console.log(priceContent)
+    // innerHTMLで、HTML要素の書き換えを行う。また、入力した金額をもとに販売手数料を計算する処理
+    priceContent.innerHTML =  Math.floor(inputValue * 0.9);
+  })
+}
+
+window.addEventListener('load', count);
+
+
