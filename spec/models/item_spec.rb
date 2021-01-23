1c10e95e4b5f05606c091f8474e118a775a98001
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-23 16:56:18 +0900

    Rubcop実行による変更点

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,99 +2,98 @@ require 'rails_helper'
 describe Item do
   before do
     @item = FactoryBot.build(:item)
-  end 
+  end
 
-  describe "出品登録" do
+  describe '出品登録' do
     context '新規登録がうまくいくとき' do
-      it "全ての情報が存在していると登録できること" do
+      it '全ての情報が存在していると登録できること' do
         expect(@item).to be_valid
-      end  
-    end 
+      end
+    end
 
     context '出品がうまくいかないとき' do
-      it "imageが空では出品できない" do
+      it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it "nameが空では出品できない" do
+      it 'nameが空では出品できない' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
 
-      it "informationが空では出品できない" do
+      it 'informationが空では出品できない' do
         @item.information = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Information can't be blank")
-      end  
+      end
 
-      it "category_idが0では出品できない" do
+      it 'category_idが0では出品できない' do
         @item.category_id = '0'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category Select")
+        expect(@item.errors.full_messages).to include('Category Select')
       end
 
-      it "sales_status_idが0では出品できない" do
+      it 'sales_status_idが0では出品できない' do
         @item.sales_status_id = '0'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sales status Select")
+        expect(@item.errors.full_messages).to include('Sales status Select')
       end
 
-      it "shipping_fee_status_idが0では出品できない" do
+      it 'shipping_fee_status_idが0では出品できない' do
         @item.shipping_fee_status_id = '0'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee status Select")
+        expect(@item.errors.full_messages).to include('Shipping fee status Select')
       end
 
-      it "prefecture_idが0では出品できない" do
+      it 'prefecture_idが0では出品できない' do
         @item.prefecture_id = '0'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture Select")
+        expect(@item.errors.full_messages).to include('Prefecture Select')
       end
 
-      it "scheduled_delivery_idが0では出品できない" do
+      it 'scheduled_delivery_idが0では出品できない' do
         @item.scheduled_delivery_id = '0'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Scheduled delivery Select")
+        expect(@item.errors.full_messages).to include('Scheduled delivery Select')
       end
 
-      it "priceが空では出品できない" do
+      it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it "priceが半角数字でなければ出品できない" do
+      it 'priceが半角数字でなければ出品できない' do
         @item.price = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number")
+        expect(@item.errors.full_messages).to include('Price Half-width number')
       end
 
-      it "priceが半角英数混合では登録できないこと" do
+      it 'priceが半角英数混合では登録できないこと' do
         @item.price = 'test3000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number")
+        expect(@item.errors.full_messages).to include('Price Half-width number')
       end
 
-      it "priceが半角英語だけでは登録できないこと" do
+      it 'priceが半角英語だけでは登録できないこと' do
         @item.price = 'test'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number")
+        expect(@item.errors.full_messages).to include('Price Half-width number')
       end
 
-      it "priceが￥300未満では出品できない" do
+      it 'priceが￥300未満では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
 
-      it "priceが￥10,000,000以上では出品できない" do
-        @item.price = 10000000
+      it 'priceが￥10,000,000以上では出品できない' do
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
-
-    end    
+    end
   end
 end
