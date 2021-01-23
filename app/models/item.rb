211ce71e6d738d694ee222ed3cb3aaf8f30d5154
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-23 16:59:02 +0900

    商品一覧表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,28 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
-  belongs_to :user 
-  
+  belongs_to :user
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
   with_options presence: true do
-    validates :image                  
-    validates :name                   
+    validates :image
+    validates :name
     validates :information
     validates :price
-  end  
-  with_options numericality: {other_than: 0, message:"Select"} do
-      validates :category_id
-      validates :sales_status_id
-      validates :shipping_fee_status_id 
-      validates :prefecture_id          
-      validates :scheduled_delivery_id  
   end
-  validates :price, numericality:{with: /\A[0-9]*\z/, message: "Half-width number"}
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  with_options numericality: { other_than: 0, message: 'Select' } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+  validates :price, numericality: { with: /\A[0-9]*\z/, message: 'Half-width number' }
+  validates :price,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'Out of setting range' }
 end
-
