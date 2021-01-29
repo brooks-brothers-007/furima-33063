c08e77a55d7cef5f8bd4fa9c52d861604aae79f4
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-29 12:19:38 +0900

    beforeアクションの追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :set_item
   before_action :move_to_ibdex
+  before_action :authenticate_user!
 
   def index
     @purchase_user = PurchaseUser.new
@@ -16,6 +17,8 @@ class PurchasesController < ApplicationController
       render :index
     end
   end
+
+  private
 
   def purchase_params
     params.require(:purchase_user).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
