7cea44b8c38339688b4e16a367950e17c790dd0c
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-29 09:43:18 +0900

    クレジットカード決済機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,21 +1,37 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @purchase_user = PurchaseUser.new  
-    @item = Item.find(params[:item_id])
+    @purchase_user = PurchaseUser.new
   end
-  
+
   def create
     @purchase_user = PurchaseUser.new(purchase_params)
     if @purchase_user.valid?
-       @purchase_user.save
-       redirect_to root_path
+      pay_item
+      @purchase_user.save
+      redirect_to root_path
     else
-       render :index
+      render :index
     end
-  end  
+  end
 
   def purchase_params
-    params.require(:purchase_user).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
-    ).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:purchase_user).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
+      item_id: params[:item_id], user_id: current_user.id, token: params[:token]
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price, 
+      card: purchase_params[:token],  
+      currency: 'jpy'                 
+    )
   end
 end
--- spec/models/purchase_user.rb
+++ spec/models/purchase_user.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 describe PurchaseUser do
   before do
-    @purchase_user = FactoryBot.build(:purchase_user)
+    @item = FactoryBot.create(:item)
+    @user = FactoryBot.create(:user)
+    sleep(1)
+    @purchase_user = FactoryBot.build(:purchase_user, user_id: @user.id, item_id: @item.id)
   end
 
   describe '購入情報登録' do
@@ -10,65 +13,92 @@ describe PurchaseUser do
         expect(@purchase_user).to be_valid
       end
     end
-   
-    context '新規登録がうまくいかないとき' do
-      it 'すべての値が正しく入力されていれば保存できること' do
-        expect(@purchase_user).to be_valid
+
+    context '購入がうまくいかないとき' do
+
+      it 'tokenが空だと購入できないこと' do
+        @purchase_user.token = nil
+        @purchase_user.valid?
+        binding.pry
+        expect(@purchase_user.errors.full_messages).to include("Token can't be blank")
       end
 
-      it 'postal_codeが空だと保存できないこと' do
+      it 'postal_codeが空だと購入できないこと' do
         @purchase_user.postal_code = nil
         @purchase_user.valid?
         expect(@purchase_user.errors.full_messages).to include("Postal code can't be blank")
       end
 
-      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと購入できないこと' do
         @purchase_user.postal_code = '1234567'
         @purchase_user.valid?
-        expect(@purchase_user.errors.full_messages).to include("Postal code Input correctly")
+        expect(@purchase_user.errors.full_messages).to include('Postal code Input correctly')
       end
 
-      it 'prefecture_idが0では出品できない' do
+      it 'prefecture_idが0では購入できない' do
         @purchase_user.prefecture_id = '0'
         @purchase_user.valid?
         expect(@purchase_user.errors.full_messages).to include('Prefecture Select')
       end
 
-      it 'cityが空だと保存できないこと' do
+      it 'cityが空だと購入できないこと' do
         @purchase_user.city = ''
         @purchase_user.valid?
         expect(@purchase_user.errors.full_messages).to include("City can't be blank")
       end
 
-      it 'addressが空だと保存できないこと' do
+      it 'addressが空だと購入できないこと' do
         @purchase_user.address = ''
         @purchase_user.valid?
         expect(@purchase_user.errors.full_messages).to include("Address can't be blank")
       end
 
-      it 'phone_numberが空だと保存できないこと' do
+      it 'phone_numberが購入だと保存できないこと' do
         @purchase_user.phone_number = ''
         @purchase_user.valid?
         expect(@purchase_user.errors.full_messages).to include("Phone number can't be blank")
       end
 
-      it 'phone_numberが12桁以上では保存できないこと' do
+      it 'phone_numberが12桁以上では購入できないこと' do
         @purchase_user.phone_number = '090123456789'
         @purchase_user.valid?
-        expect(@purchase_user.errors.full_messages).to include("Phone number Input only number")
-      end  
-
-      it 'phone_numberが英文字だと保存できないこと' do
-        @purchase_user.phone_number = 'abcdefghijk'
-        @purchase_user.valid?
-        expect(@purchase_user.errors.full_messages).to include("Phone number Input only number")
+        expect(@purchase_user.errors.full_messages).to include('Phone number Input only number')
       end
 
-      it 'phone_numberが半角英数混合だと保存できないこと' do
+      it 'phone_numberにハイフンが含まれていると購入できないこと' do
+        @purchase_user.phone_number = '090-1234-5678'
+        @purchase_user.valid?
+        expect(@purchase_user.errors.full_messages).to include('Phone number Input only number')
+      end
+
+      it 'phone_numberが英文字だと購入できないこと' do
+        @purchase_user.phone_number = 'ABCDEFGHIJK'
+        @purchase_user.valid?
+        expect(@purchase_user.errors.full_messages).to include('Phone number Input only number')
+      end
+
+      it 'phone_numberが半角英語だと購入できないこと' do
+        @purchase_user.phone_number = 'abcdefghijk'
+        @purchase_user.valid?
+        expect(@purchase_user.errors.full_messages).to include('Phone number Input only number')
+      end
+
+      it 'phone_numberが半角英数混合だと購入できないこと' do
         @purchase_user.phone_number = '090abcdefgh'
         @purchase_user.valid?
-        binding.pry
-        expect(@purchase_user.errors.full_messages).to include("Phone number Input only number")
+        expect(@purchase_user.errors.full_messages).to include('Phone number Input only number')
+      end
+
+      it 'userが紐づいていないと保存できないこと' do
+        @purchase_user.user_id = nil
+        @purchase_user.valid?
+        expect(@purchase_user.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'userが紐づいていないと保存できないこと' do
+        @purchase_user.item_id = nil
+        @purchase_user.valid?
+        expect(@purchase_user.errors.full_messages).to include("Item can't be blank")
       end
     end
   end
