7d47297c9470434a05bb504d3834005eba1ef390
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-29 17:14:29 +0900

    条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to item_path if current_user.id != @item.user_id
+    redirect_to item_path if user_signed_in? && current_user.id != @item.user_id || !@item.purchase.nil?
   end
 
   def update
